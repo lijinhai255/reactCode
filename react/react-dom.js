ebc03f6fd345740574664ac39a0c523fdac2e88d
lijinhai255 <18854109500@163.com>
2024-10-04 07:08:52 +0800

    add:fiberNode

--- react/react-dom.js
+++ react/react-dom.js
@@ -37,12 +37,12 @@ export function createDome(fiber) {
  *
  */
 function workloop(deadline) {
-  console.log(params, "parmas");
   // 停止标识
   let shouldYield = false;
   while (nextUnitOfWork && !shouldYield) {
     // 执行工作单元
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    console.log(nextUnitOfWork, "nextUnitOfWork-nextUnitOfWork");
     // 判断是否需要停止
     // 模拟的情况是咱们自己还得判断下有没有16.67
     shouldYield = deadline.timeRemaining() < 1;
@@ -51,6 +51,48 @@ function workloop(deadline) {
 
 requestIdleCallback(workloop);
 
-function performUnitOfWork(fiber) {}
+function performUnitOfWork(fiber) {
+  console.log("fiber", fiber);
+  if (fiber.dom == null) {
+    fiber.dom = createDome(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+  const elements = fiber.props.children;
+
+  // 索引 index = 0
+  let index = 0;
+  // 上一个兄弟节点
+  let prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    // 将第一个孩子节点设置为fiber的字节点
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else if (element) {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+}
 // 超级fiber  的事件
 // render  ->  requestIdleCallback ->  workloop -> nextUnitOfWork -> performUnitOfWork
